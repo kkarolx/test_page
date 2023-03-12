2a12f84fe9e8301387a93c72ef35df413064464e
kkarolx <kkrzysztofeklyreco@gmail.com>
2023-03-12 10:25:28 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const galleryButton = document.querySelector('button:nth-of-type(1)');
+const moreArtworkButton = document.querySelector('button:nth-of-type(2)');
+const resumeButton = document.querySelector('button:nth-of-type(3)');
+
+galleryButton.addEventListener('click', function() {
+  // Code to display gallery goes here
+});
+
+moreArtworkButton.addEventListener('click', function() {
+  // Code to load more artwork goes here
+});
+
+resumeButton.addEventListener('click', function() {
+  // Code to display resume goes here
+});
